c3cfaded27734655521f023e147c72f16c010350
Patrizio Bruno <desertconsulting@gmail.com>
2016-09-30 22:07:03 +0200

    submodule install automatized

--- install-modules.sh
+++ install-modules.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
 for d in authenticatedws broker collector dispatcher publisher subscriber; do
+    printf "=== Installing %s\n" "$d"
 	cd $d && npm install
 	cd ..
+	printf "=== Installation exit code: %s for %s\n" $? "$d"
 done
 
